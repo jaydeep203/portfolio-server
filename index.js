be452138d4c92c33bbaeb6983e05fe2763eeda54
Jaydeep Deshpande <mrjaydeep2003@gmail.com>
2024-07-24 19:11:38 +0530

    dotenv added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+
 app.use(cors());
+dotenv.config();
 
 const project = require("./routes/project");
 
@@ -11,4 +15,4 @@ const project = require("./routes/project");
 app.use("/api/v1/", project);
 
 
-app.listen(4000);
+app.listen(PORT);
